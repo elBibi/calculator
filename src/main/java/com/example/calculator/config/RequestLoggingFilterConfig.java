00e56038706e0d8999175ccbb7f859525a66e625
elBibi <my.ajt.knight@gmail.com>
2024-06-12 12:44:47 -0400

    Added Logging so request could be seen in Console

---
+++ src/main/java/com/example/calculator/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,21 @@
+package com.example.calculator.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(true);  // You can set this to false if headers are not needed
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
+    }
+}
